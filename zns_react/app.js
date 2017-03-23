fa516b5d536110e120afea2accb5da0e218284bb
fengjie.liu <fengjie.liu@chinascopefinancial.com>
2017-03-23 15:10:11 +0800

    add zns_react

---
+++ zns_react/app.js
@@ -0,0 +1,12 @@
+class MyTitle extends React.Component {
+    render() {
+        return <div >
+            < h3 > welcome react < /h3><h4>welcome h4</h
+        4 >
+            < /div>
+    }
+}
+
+ReactDOM.render( < MyTitle / > ,
+    document.querySelector('#div1')
+);
---
+++ zns_react/app.jsx
@@ -0,0 +1,9 @@
+class MyTitle extends React.Component {
+    render() {
+        return <div><h3> welcome react < /h3><h4>welcome h4</h4>< /div>
+    }
+}
+
+ReactDOM.render( < MyTitle / > ,
+    document.querySelector('#div1')
+);
